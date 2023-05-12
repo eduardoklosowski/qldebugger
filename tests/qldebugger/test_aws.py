59d080886759bd6e9d697379fbc2d1167ad79467
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2023-05-12 13:53:46 -0300

    Refactor tests of get_client

--- tests/qldebugger/test_aws.py
+++ tests/qldebugger/test_aws.py
@@ -1,14 +1,17 @@
 from unittest.mock import Mock, patch
 
+import pytest
+
 from qldebugger.aws import get_client, inject_aws_config_in_client
 from qldebugger.config.file_parser import ConfigAWS
 from tests.utils import randstr
 
 
 class TestGetClient:
+    @pytest.mark.parametrize('service', ['sqs', 'sts'])
     @patch('qldebugger.aws.get_config')
     @patch('qldebugger.aws.boto3')
-    def test_get_sqs_client(self, mock_boto3: Mock, mock_get_config: Mock) -> None:
+    def test_run(self, mock_boto3: Mock, mock_get_config: Mock, service: str) -> None:
         aws_config = ConfigAWS(
             profile=randstr(),
             access_key_id=randstr(),
@@ -20,7 +23,7 @@ class TestGetClient:
 
         mock_get_config.return_value.aws = aws_config
 
-        returned = get_client('sqs')
+        returned = get_client(service)  # type: ignore[call-overload]
 
         mock_boto3.Session.assert_called_once_with(
             aws_access_key_id=aws_config.access_key_id,
@@ -30,10 +33,53 @@ class TestGetClient:
             profile_name=aws_config.profile,
         )
         mock_boto3.Session.return_value.client.assert_called_once_with(
-            service_name='sqs',
+            service_name=service,
             endpoint_url=aws_config.endpoint_url,
         )
         assert returned == mock_boto3.Session.return_value.client.return_value
+
+    @patch('qldebugger.aws.get_config')
+    @patch('qldebugger.aws.boto3')
+    def test_cache(self, mock_boto3: Mock, mock_get_config: Mock) -> None:
+        aws_config = ConfigAWS(
+            profile=randstr(),
+            access_key_id=randstr(),
+            secret_access_key=randstr(),
+            session_token=randstr(),
+            region=randstr(),
+            endpoint_url=randstr(),
+        )
+
+        mock_get_config.return_value.aws = aws_config
+        mock_boto3.Session.return_value.client.side_effect = lambda **kwargs: object()
+
+        get_client.cache_clear()  # type: ignore[attr-defined]
+
+        returned1 = get_client('sts')
+
+        cache_info = get_client.cache_info()  # type: ignore[attr-defined]
+        assert cache_info.hits == 0
+        assert cache_info.misses == 1
+        assert mock_boto3.Session.call_count == 1
+        assert mock_boto3.Session.return_value.client.call_count == 1
+
+        returned2 = get_client('sts')
+
+        cache_info = get_client.cache_info()  # type: ignore[attr-defined]
+        assert cache_info.hits == 1
+        assert cache_info.misses == 1
+        assert mock_boto3.Session.call_count == 1
+        assert mock_boto3.Session.return_value.client.call_count == 1
+        assert returned1 is returned2
+
+        returned3 = get_client('sqs')
+
+        cache_info = get_client.cache_info()  # type: ignore[attr-defined]
+        assert cache_info.hits == 1
+        assert cache_info.misses == 2
+        assert mock_boto3.Session.call_count == 2
+        assert mock_boto3.Session.return_value.client.call_count == 2
+        assert returned3 is not returned1  # type: ignore[comparison-overlap]
 
 
 class TestInjectAwsConfigInClient:
