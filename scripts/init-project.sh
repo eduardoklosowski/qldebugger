e28ef23fd981e3b55e7664730f821eeef03dcd47
Eduardo Klosowski <eduardo_klosowski@yahoo.com>
2024-09-03 23:39:35 -0300

    Refactor devcontainer

--- scripts/init-project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-# Project dependencies
-poetry install --with=docs
-
-# Visual Studio Code
-[ -e .vscode/settings.json ] || jq ".[\"python.defaultInterpreterPath\"] = \"$(poetry env info -e)\"" .vscode/settings.json.example > .vscode/settings.json
